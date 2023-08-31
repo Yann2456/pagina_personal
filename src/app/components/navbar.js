9bb40efe15b88098c03f8e1f8cdb0086026c549f
Baco CCB <Baco@ccb.edu.gt>
2023-08-31 10:28:05 -0600

    Extra

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -5,23 +5,31 @@ import styles from './navar.module.css';
 function Navbar(){
     const [selectedItem, setSelectedItem] = useState('Inicio');
 
-    const hadleItemClick = (item) => {
+    function hadleItemClick(item, id) {
         setSelectedItem(item);
+        scrollToSection(id);
+    }
+
+    const scrollToSection = (id) => {
+        const section = document.getElementById(id);
+        if (section) {
+            section.scrollIntoView({behavior: 'smooth'});
+        }
     };
 
     return(
         <nav className={styles.navbar}>
             <div 
-                className={styles.navbarItem + `${selectedItem === 'Inicio' ? styles.selected :''}`}
-                onClick={() => handleItemClick('Inicio')}
+                className={styles.navbarItem + ` ${selectedItem === 'Inicio' ? styles.selected :''}`}
+                onClick={() => hadleItemClick('Inicio', 'header')}
             >
                 <a href="#header">Inicio</a>
             </div>
             <div 
-                className={styles.navbarItem + `${selectedItem === 'Skills' ? styles.selected :''}`}
-                onClick={() => handleItemClick('Skills')}
+                className={styles.navbarItem + ` ${selectedItem === 'Skills' ? styles.selected :''}`}
+                onClick={() => hadleItemClick('Skills', 'info')}
             >
-            <a href="#info">Skills</a>
+            <a href="#info">Servicios</a>
             </div>
         </nav>
     );
